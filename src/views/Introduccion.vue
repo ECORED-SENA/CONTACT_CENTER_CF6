adf4317300e24638fda96d52218158d20a8393e7
Jhon Jairo Urueta Alvarez <jhon.urueta@misena.edu.co>
2021-10-22 16:51:16 -0500

    Correcciones 4
    Se actualiza video de introducción y se genera material.zip

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -11,7 +11,7 @@
     p.mb-5 El siguiente video nos hace una presentación general de lo que estudiaremos en el presente componente formativo.
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/y3NqB6wUkuE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     p.mb-0 Para la elaboración de este componente, se abordaron varios autores conocidos en cierre de ventas, de quienes se han citado y referenciado conceptos y ejemplos para los fines educativos de esta materia, en el entendido de que el conocimiento es social y, por lo tanto, es para ser usado por quienes necesitan adquirirlo. Se espera que este documento sea útil para todos aquellos, aprendices y lectores en general, que estén interesados en acercarse a asuntos básicos de la operación de servicios en contact center y BPO.
 
 </template>
